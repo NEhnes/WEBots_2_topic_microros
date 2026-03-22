c7989500376343b399e80a5f370fcb5a14c553b1
NathanUbuntu <=>
2026-03-21 21:24:13 -0400

    defined message struct and client object for service implementation. still having issues with data reaching the ros2 service node i wrote

--- .pio/build/esp32/FrameworkArduino/HWCDC.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32/FrameworkArduino/HWCDC.cpp.o: \
- /home/nehnes/.platformio/packages/framework-arduinoespressif32/cores/esp32/HWCDC.cpp \
- /home/nehnes/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
- /home/nehnes/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
